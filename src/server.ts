2b9b6c483d2af2a579cb722d8304e5769b17f071
Caio Maia <caio.maiasb@gmail.com>
2021-06-22 19:44:35 -0300

    Utilizando o gitignore

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
 import express, { json } from 'express';
+import "reflect-metadata";
+import "./database";
 
 const app = express();
-
-app.use(express.json());
-
-app.get("/test", (request, response) => {
-    return response.json({ Message: "Test" });
-});
-
-app.post("/test-post", (request, response) => {
-    const { name, email } = request.body;
-
-    const user = { name, email };
-    return response.status(200).json(user);
-});
 
 app.listen(3000, () => console.log("Server is running!"));
