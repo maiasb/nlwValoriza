c798c8e86f3a1ab3b1793f12278d02fac69042d4
Caio Maia <caio.maiasb@gmail.com>
2021-06-22 18:48:11 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { json, request } from 'express';
+
+const app = express();
+
+app.get("/test", (request, response) => {
+    return response.json({ Message: "Test" });
+});
+
+app.post("/test-post", (request, response) => {
+    const { name, email } = request.body;
+
+    const user = { name, email };
+
+    return response.status(200).json(user);
+});
+
+app.listen(3000, () => console.log("Server is running!"));
