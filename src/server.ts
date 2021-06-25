1662824da2c5c1ced8f4dce65499e3d56781bdc9
Caio Maia <caio.maiasb@gmail.com>
2021-06-25 20:51:25 -0300

    Criação de middleware para a criação da coluna password do usuário e adicionando na entidade o password; debug funcionando;

--- src/server.ts
+++ src/server.ts
@@ -20,4 +20,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     });
 });
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3000, () => console.log("Server is running on port 3000!"));
--- src/services/createUserService.ts
+++ src/services/createUserService.ts
@@ -5,11 +5,12 @@ interface IUserRequest {
     name: string;
     email: string;
     admin?: boolean;
+    password: string
 }
 
 class CreateUserService {
 
-    async execute({ name, email, admin }: IUserRequest) {
+    async execute({ name, email, admin, password }: IUserRequest) {
         const usersRepository = getCustomRepository(usersRepositories);
 
         // throw new devolve resposta a camada acima (Aqui o controller)
@@ -23,7 +24,7 @@ class CreateUserService {
             throw new Error("User already exists.");
         }
 
-        const user = usersRepository.create({ name, email, admin })
+        const user = usersRepository.create({ name, email, admin, password })
 
         await usersRepository.save(user);
 
