a667534d0aca6d4b3944d1000955a8fcfbc2aa7c
Caio Maia <caio.maiasb@gmail.com>
2021-06-28 20:28:08 -0300

    response no controller estava 'responde';

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -2,7 +2,7 @@ import { Request, response, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
-    async handle(request: Request, responde: Response) {
+    async handle(request: Request, response: Response) {
         const { email, password } = request.body;
 
         const authenticateUserService = new AuthenticateUserService();
